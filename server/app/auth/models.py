09ce386926b90ed99339382f023449353b420eed
B4TRUH4 <matveybond2002@gmail.com>
2024-08-10 02:23:48 +0700

    added endpoint for making user an administrator

--- server/app/auth/models.py
+++ server/app/auth/models.py
@@ -1,9 +1,14 @@
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import Integer, Column
+from sqlalchemy.orm import relationship
+
 from app.models.base import Base
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
+    """Таблица пользователей"""
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True)
+
+    articles = relationship("Article", back_populates="author")
+    comments = relationship("Comment", back_populates="author")
--- server/app/auth/routers.py
+++ server/app/auth/routers.py
@@ -1,8 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .managers import UserManager
+from .models import User
 from .schemas import UserCreate, UserRead, UserUpdate
-from .dependencies import fastapi_users, auth_backend
+from .dependencies import (
+    fastapi_users,
+    auth_backend,
+    get_user_manager,
+    current_active_superuser)
+from ..dependencies import get_async_session
 
 router = APIRouter()
+
+users_router = fastapi_users.get_users_router(UserRead, UserUpdate)
+
+
+@users_router.post("/{id}/make-superuser", response_model=UserRead)
+async def make_superuser(
+        user_id: int,
+        user_manager: UserManager = Depends(get_user_manager),
+        db: AsyncSession = Depends(get_async_session),
+        superuser: User = Depends(current_active_superuser)):
+    """Эндпоинт для предоставления прав администратора"""
+
+    user = await user_manager.get(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    user.is_superuser = True
+    await db.commit()
+    return user
 
 router.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -20,7 +50,7 @@ router.include_router(
 )
 
 router.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
+    users_router,
     prefix="/users",
     tags=["users"],
 )
