3106d7ae15b57d4a264a2a2d7991c36819c6744d
B4TRUH4 <matveybond2002@gmail.com>
2024-08-10 15:14:09 +0700

    created endpoints for ban/unban user

--- server/app/auth/routers.py
+++ server/app/auth/routers.py
@@ -8,7 +8,8 @@ from .dependencies import (
     fastapi_users,
     auth_backend,
     get_user_manager,
-    current_active_superuser)
+    current_active_superuser
+)
 from ..dependencies import get_async_session
 
 router = APIRouter()
@@ -21,7 +22,7 @@ async def make_superuser(
         user_id: int,
         user_manager: UserManager = Depends(get_user_manager),
         db: AsyncSession = Depends(get_async_session),
-        superuser: User = Depends(current_active_superuser)):
+        _: User = Depends(current_active_superuser)):
     """Эндпоинт для предоставления прав администратора"""
 
     user = await user_manager.get(user_id)
@@ -33,6 +34,59 @@ async def make_superuser(
     user.is_superuser = True
     await db.commit()
     return user
+
+
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"]
+)
+
+
+@users_router.post("/{id}/ban", response_model=UserRead)
+async def make_superuser(
+        user_id: int,
+        user_manager: UserManager = Depends(get_user_manager),
+        db: AsyncSession = Depends(get_async_session),
+        _: User = Depends(current_active_superuser)):
+    """Эндпоинт для бана пользователя"""
+
+    user = await user_manager.get(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    user.is_active = False
+    await db.commit()
+    return user
+
+
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"]
+)
+
+
+@users_router.post("/{id}/unban", response_model=UserRead)
+async def make_superuser(
+        user_id: int,
+        user_manager: UserManager = Depends(get_user_manager),
+        db: AsyncSession = Depends(get_async_session),
+        _: User = Depends(current_active_superuser)):
+    """Эндпоинт для разбана пользователя"""
+
+    user = await user_manager.get(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    user.is_active = True
+    await db.commit()
+    return user
+
 
 router.include_router(
     fastapi_users.get_auth_router(auth_backend),
